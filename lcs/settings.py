d823b7751af377f1e3d50faf855d7efe5c51d316
charith kabamba <ledouxkabamba135@@gmail.com>
2026-01-12 21:30:23 +0100

    first commit2

--- lcs/settings.py
+++ lcs/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+# import django decouple for environment variables
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_wlrvqb(&8%x7y9^)i0x34$k4llfilr2(z6a$%ib_uu2jpqzc'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-i_wlrvqb(&8%x7y9^)i0x34$k4llfilr2(z6a$%ib_uu2jpqzc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,9 +91,7 @@ WSGI_APPLICATION = 'lcs.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+    'default': dj_database_url.config('DATABASE_URL'
     )
 }
 
